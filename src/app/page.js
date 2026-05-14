f61df01d0f65c9f425777995efbd7acd59e495ec
Gabriel Fuentes <161405047+Gabriel-Fuentes-de-Freitas-Yamashita@users.noreply.github.com>
2026-05-14 12:21:55 +0000

    css global alterado para tirar o sublinhado dos card, adicionnado o botao da lista na home e sua logica, antes da sua funcionbilidade

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use client"
+                                                                                                "use client"
 import { useState } from 'react';
 import { produtos } from "../data/produtos";
 import { mercados } from "../data/mercados";
